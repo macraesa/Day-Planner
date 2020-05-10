e776ceb25d05d1ffadc62643383f693e3f63b2d7
Scott Mac <macraesa@gmail.com>
2020-05-09 22:49:40 -0400

    local storage on one line

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -6,42 +6,39 @@
 // <div id="hour3"></div>
 // <div id="hour4"></div>
 
-// for (var i = 0; i < 5; i++) {
-//     $("#hour" + i).text("You are in hour " + i)
-
-// }
 
 
 
-
-// var textEl = document.querySelector(".description")
-// var saveBtnEL = document.querySelector(".saveBtn")
-
-// var count = localStorage.getItem("count")
-
-// textEl.textContent = count
-
-// saveBtnEL.addEventListener("click", function() {
-
-//     localStorage.setItem("count", count)
-// })
 
 $(document).ready(function() {
 
-    var currentDateAndTime = Date(Date.now());// exp: Fri Oct 25 2019 17:40:56 GMT-0400 (Eastern Daylight Time)`
-    $("#currentDay").append(currentDateAndTime)
-    // var currentHour = new Date().getHours();//Current hour in military time
-
     //place all of your javascript code here
-    var test = 5;
-    console.log("testing")
+    for (var i = 0; i < 1; i++) {
+        $("#hour" + i).text("You are in hour " + i)
+    }
+
+    var Time = Date(Date.now());// exp: Fri Oct 25 2019 17:40:56 GMT-0400 (Eastern Daylight Time)`
+    $("#currentDay").append(Time)
+    var currentHour = new Date().getHours();//Current hour in military time
+    $(".description").append(currentHour)
     
     
-    $(".btn").on("click", function() {
-        var time = $(this).val()
-        console.log(time)
-        var storeContent = $("#textArea" + time).val().trim()
-        localStorage.getItem("storeContent")
+    
+    
+    
+    $(".btn").click(function() {
+
+    
+
+            
+            var descriptEl = $(".description").val()
+            localStorage.setItem("description", JSON.stringify(descriptEl))
+            
+            localStorage.getItem("description")
+        
+
     })
+
+    
     
 })
